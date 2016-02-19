d17e73392c343839e484dae2b9de0236eaad1849
alex <alexanderlopez@outlook.com>
2016-02-19 13:46:13 -0600

    commit inicial

---
+++ server.js
@@ -0,0 +1,141 @@
+// Required Modules
+var express    = require("express");
+var morgan     = require("morgan");
+var bodyParser = require("body-parser");
+var jwt        = require("jsonwebtoken");
+var mongoose   = require("mongoose");
+var app        = express();
+
+var port = process.env.PORT || 3001;
+var User     = require('./models/User');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/usuarios', function(err, res) {
+        if(err) {
+                console.log('ERROR: no se realizo la conexion a la db. ' + err);
+        } else {
+                console.log('Base de datos conectada con exito!');
+        }
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
+
+
+app.post('/authenticate', function(req, res) {
+    User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
+        if (err) {
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            });
+        } else {
+            if (user) {
+               res.json({
+                    type: true,
+                    data: user,
+                    token: user.token
+                });
+            } else {
+                res.json({
+                    type: false,
+                    data: "Incorrect email/password"
+                });
+            }
+        }
+    });
+});
+
+
+app.post('/signin', function(req, res) {
+    User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
+        if (err) {
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            });
+        } else {
+            if (user) {
+                res.json({
+                    type: false,
+                    data: "User already exists!"
+                });
+            } else {
+                var userModel = new User();
+                userModel.email = req.body.email;
+                userModel.password = req.body.password;
+                userModel.save(function(err, user) {
+                    user.token = jwt.sign(user, process.env.JWT_SECRET || 'contrasenasecreta');
+                    user.save(function(err, user1) {
+                        res.json({
+                            type: true,
+                            data: user1,
+                            token: user1.token
+                        });
+                    });
+                })
+            }
+        }
+    });
+});
+
+app.get('/me', ensureAuthorized, function(req, res) {
+    User.findOne({token: req.token}, function(err, user) {
+        if (err) {
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            });
+        } else {
+            res.json({
+                type: true,
+                data: user
+            });
+        }
+    });
+});
+app.get('/test', ensureAuthorized, function(req, res) {
+    User.findOne({token: req.token}, function(err, user) {
+        if (err) {
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            });
+        } else {
+            res.json({
+                type: true,
+                data: user
+            });
+        }
+    });
+});
+
+function ensureAuthorized(req, res, next) {
+    var bearerToken;
+    var bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        var bearer = bearerHeader.split(" ");
+        bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.send(403);
+    }
+}
+
+process.on('uncaughtException', function(err) {
+    console.log(err);
+});
+
+// Start Server
+app.listen(port, function () {
+    console.log( "Express server listening on port " + port);
+});
